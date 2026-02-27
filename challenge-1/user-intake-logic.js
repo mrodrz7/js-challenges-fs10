1fc74de99e8418dff95967b5f2d2b90da0040859
Martin Rodriguez <m.rodriguez0743@gmail.com>
2026-02-26 23:56:09 -0500

    Add logic for steps 2-4

--- challenge-1/user-intake-logic.js
+++ challenge-1/user-intake-logic.js
@@ -1,7 +1,38 @@
 let firstName = "John"
 let age = 34
 let annualIncome = 50000
-let creditScore = 680
+let creditScore = 650
 let isCitizen = true
 let hasCriminalRecord = false
+let accountStatus = false;
 
+if (age >= 17 && isCitizen && !hasCriminalRecord) {
+    accountStatus = true;
+    console.log("Account Approved")
+} else {
+    console.log("Account Denied")
+}
+
+if (annualIncome > 100000 && creditScore >= 720 && accountStatus) {
+    console.log("Premium Approved")
+} else {
+    console.log("Standard Account Only")
+}
+
+if (creditScore < 600 || annualIncome < 25000) {
+    console.log("Flag for Review")
+} else {
+    console.log("No Risk Flags")
+}
+
+let riskLevel;
+
+if (creditScore < 550) {
+    riskLevel = "High"
+} else if (creditScore >= 550 && creditScore <= 650) {
+    riskLevel = "Medium"
+} else {
+    riskLevel = "Low"
+}
+
+console.log(`${firstName}, your credit risk level is ${riskLevel}`)
